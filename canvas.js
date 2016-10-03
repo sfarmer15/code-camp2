d15d5d6452735f3d52da94973163d32eb0ecf1de
Samantha Farmer <samanthafarmer@Samanthas-MacBook-Pro.local>
2016-10-02 21:44:00 -0400

    added draw circle function

--- canvas.js
+++ canvas.js
@@ -2,9 +2,16 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
 
-ctx.beginPath();
-ctx.arc(150,150,80,0,2*Math.PI);
-ctx.strokeStyle="red";
-ctx.fillStyle="yellow";
-ctx.stroke();
-ctx.fill();
+drawCircle(200, 300, 40);
+drawCircle();
+
+
+
+function drawCircle(x, y, r) {
+    ctx.beginPath();
+    ctx.arc(x||150,y||150,r||80,0,2*Math.PI);
+    ctx.strokeStyle="red";
+    ctx.fillStyle="yellow";
+    ctx.stroke();
+    ctx.fill();
+}
