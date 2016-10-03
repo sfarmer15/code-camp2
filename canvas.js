b5e193de1cec57a933b41917f459c36d916d86e3
Samantha Farmer <samanthafarmer@Samanthas-MacBook-Pro.local>
2016-10-02 21:55:30 -0400

    The circle moves...

--- canvas.js
+++ canvas.js
@@ -1,12 +1,17 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-
-drawCircle(200, 300, 40, "black", "white");
-drawCircle();
-
+var circle1x=200;
+setInterval(update, 100);
 
 
+
+function update() {
+    ctx.clearRect(0,0,c.width,c.height)
+    drawCircle(circle1x, 300, 40, "black", "white");
+    drawCircle();
+    circle1x+=5;
+}
 function drawCircle(x, y, r, stroke, fill) {
     ctx.beginPath();
     ctx.arc(x||150,y||150,r||80,0,2*Math.PI);
