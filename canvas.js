45520067e964ba0efe803a1f06e38d2a79105ec6
Samantha Farmer <samanthafarmer@Samanthas-MacBook-Pro.local>
2016-10-02 21:47:44 -0400

    added stroke and fill parameters

--- canvas.js
+++ canvas.js
@@ -2,16 +2,16 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
 
-drawCircle(200, 300, 40);
+drawCircle(200, 300, 40, "black", "white");
 drawCircle();
 
 
 
-function drawCircle(x, y, r) {
+function drawCircle(x, y, r, stroke, fill) {
     ctx.beginPath();
     ctx.arc(x||150,y||150,r||80,0,2*Math.PI);
-    ctx.strokeStyle="red";
-    ctx.fillStyle="yellow";
+    ctx.strokeStyle=stroke||"red";
+    ctx.fillStyle=fill||"yellow";
     ctx.stroke();
     ctx.fill();
 }
