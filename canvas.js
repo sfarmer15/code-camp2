9f61a4057ce04fb111ca956af3e1ccf622f5c93e
Samantha Farmer <samanthafarmer@Samanthas-MacBook-Pro.local>
2016-10-08 20:15:14 -0400

    Changed my inputs to objects

--- canvas.js
+++ canvas.js
@@ -2,21 +2,42 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
 var circle1x=200;
+var circle1={
+    x: 200,
+    y: 300,
+    radius: 40,
+    stroke: "black",
+    fill: "white"
+};
+var circle2={
+    x: 250,
+    y: 250,
+    radius: 53,
+    stroke: "green",
+    fill: "pink"
+};
 setInterval(update, 100);
 
 
 
 function update() {
-    ctx.clearRect(0,0,c.width,c.height)
-    drawCircle(circle1x, 300, 40, "black", "white");
+    ctx.clearRect(0,0,c.width,c.height);
+    drawCircle(circle1);
     drawCircle();
-    circle1x+=5;
+    drawCircle(circle2);
+    circle1.x+=5;
 }
-function drawCircle(x, y, r, stroke, fill) {
+function drawCircle(circle) {
+    circle=circle||{};
+    var x=circle.x||150;
+    var y=circle.y||150;
+    var radius=circle.radius||80;
+    var fill=circle.fill||"red";
+    var stroke=circle.stroke||"yellow";
     ctx.beginPath();
-    ctx.arc(x||150,y||150,r||80,0,2*Math.PI);
-    ctx.strokeStyle=stroke||"red";
-    ctx.fillStyle=fill||"yellow";
+    ctx.arc(x,y,radius,0,2*Math.PI);
+    ctx.strokeStyle=stroke;
+    ctx.fillStyle=fill;
     ctx.stroke();
     ctx.fill();
 }
