ed2ea91901d7bfb849fd8d53e2f6a1f5582ee012
Samantha Farmer <samanthafarmer@Samanthas-MacBook-Pro.local>
2016-10-09 13:39:56 -0400

    Circles bounce + add new circle with click

--- canvas.js
+++ canvas.js
@@ -6,17 +6,29 @@ var circles=[];
 setInterval(update, 100);
 
 document.addEventListener("keydown", onKeyDown);
+document.addEventListener("click", onClick);
 
 
 function update() {
     ctx.clearRect(0,0,c.width,c.height);
-    for (var i=0; i<circles.length; i++){
-        drawCircle(circles[i]);
-        circles[i].x+=circles[i].dx;
-        circles[i].y+=circles[i].dy;
+    for (var i=0, circle; i<circles.length; i++){
+        circle=circles[i];
+        drawCircle(circle);
+        switch (true){
+            case (circle.x-circle.radius)<=0 || (circle.x+circle.radius)>=c.width:
+                circle.dx = -circle.dx;
+                circle.dy = -circle.dy;
+            case (circle.y-circle.radius)<=0 || (circle.y+circle.radius)>=c.height:
+                circle.dy = -circle.dy;
+        };
+       
+            circle.x+=circle.dx;
+            circle.y+=circle.dy;
+        };
+        
     };
-    //drawCircle();
-}
+
+
 function drawCircle(circle) {
     circle=circle||{};
     var x=circle.x||150;
@@ -30,19 +42,32 @@ function drawCircle(circle) {
     ctx.fillStyle=fill;
     ctx.stroke();
     ctx.fill();
-}
+};
 function onKeyDown(event) {
     if(event.keyCode==32) {
         event.preventDefault();
-        console.log("you pressed the spacebar")
+        console.log("you pressed the spacebar");
         circles.push({
             x: Math.floor(Math.random()*c.width),
             y: Math.floor(Math.random()*c.height),
-            radius: Math.floor(Math.random()*0.5*c.height),
+            radius: r=Math.floor(Math.random()*40),
             stroke: "rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")",
             fill: "rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")",
-            dx: Math.floor(Math.random()*10)-5,
-            dy: Math.floor(Math.random()*20)-10
+            dx: Math.floor(Math.random())*r+5,
+            dy: Math.floor(Math.random())*r-5,
         });
     }
+};
+function onClick(click){
+    console.log(click);
+    circles.push({
+        x:click.layerX,
+        y:click.layerY,
+        radius: r=Math.floor(Math.random()*40),
+        stroke: "rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")",
+        fill: "rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")",
+        dx: Math.floor(Math.random())*r+5,
+        dy: Math.floor(Math.random())*r-5,
+    })
+
 }
